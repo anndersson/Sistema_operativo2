df5e6f6849ec75b05bacf606af50dd16d59ff73a
Andersson <aonavas@scl.edu.gt>
2022-05-26 21:59:13 -0600

    terminado

--- js/Main.js
+++ js/Main.js
@@ -1,11 +1,38 @@
 // etiquetas del index
-var menu = document.getElementById('menu');
-var ico_menu = document.getElementById('ico_menu');
+
+
 
 //FUNCION
 function activar(){
-    menu.classList.add("activar");
+    var menu = document.getElementById('menu');
+    
+
+   
+    menu.classList.remove("menu1");
+}
+
+function ocultar(){
+    var menu = document.getElementById('menu');
+
+    
+
+    menu.classList.add("menu1");
+
+}
+
+function activar_2(){
+    var spotify = document.getElementById('spotify');
+
+    spotify.classList.remove("activar_2")
+    
+}
+
+function cerrar(){
+    var spotify = document.getElementById('spotify');
+
+    spotify.classList.add("activar_2")
+    
 }
 
 //Evento
-ico_menu.addEventListener('click',activar);
+ico_menu2.addEventListener("click",ocultar)
